953f011751af2580633c6fe6d9c8862e273a311b
Rich Hildred <rhildred@gmail.com>
2020-09-15 11:02:54 -0400

    added nav

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 import "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js";
 import "https://cdnjs.cloudflare.com/ajax/libs/marked/1.1.1/marked.js";
+import "https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js";
 import aPages from "../pages/index.js";
 
 class Page {
@@ -34,9 +35,10 @@ class Article extends Page{
     }
 }
 
+const sName = "Richard Hildred";
+
 class Footer extends Page{
     render(){
-        const sName = "Richard Hildred";
         const yToday = new Date().getFullYear();
         $("footer").html(
             `&copy; ${yToday} ${sName}`
@@ -44,11 +46,41 @@ class Footer extends Page{
     }
 }
 
+class Nav extends Page{
+    render(){
+        let sMenu = "";
+        for(let n = 0; n < aPages.length; n++){
+            sMenu += `<li><a href="#${aPages[n].title}">${aPages[n].title}</a></li>`;
+        }
+
+        $("nav").html(`
+        <div class="navbar navbar-inverse navbar-static-top" role="navigation">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#home">Portfolio of ${sName}</a>
+        </div>
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-right">
+                ${sMenu}
+            </ul>
+        </div>
+
+    </div>
+
+        `);
+    }
+}
+
 class Portfolio extends Page{
     constructor(){
         super();
         this.header = new Page();
-        this.nav = new Page();
+        this.nav = new Nav();
         this.article = new Article();
         this.footer = new Footer();
     }
