f9cc50d31ad21fe9965feb951bc3385defd4d4f9
Rich Hildred <rhildred@gmail.com>
2020-09-22 09:39:52 -0400

    switch among alternative items.

--- js/index.js
+++ js/index.js
@@ -15,10 +15,12 @@ class Items extends Page{
         super();
         this.oItems = oItems;
         this.nCurrentItem = 0;
-        $(".itemLink").on("click", (evt) =>{
+        $("article#items").click((evt) =>{
             evt.preventDefault();
-            alert("clicked " + this.oItems[this.nCurrentItem].title);
-
+            this.nCurrentItem = evt.target.id[4];
+            $("article#current").html("");
+            $("article#items").html("");
+            this.render();
         });
     }
     render(){
@@ -31,12 +33,11 @@ class Items extends Page{
             )
 
         })
-
         for(let n = 0; n < this.oItems.length; n++){
             if(n != this.nCurrentItem){
                 $("article#items").append(`
-                <div class="item"><a id="item${n}" class="itemLink" href="#">
-                <img src="${this.oItems[n].specialImage}" /></a></div>
+                <div class="item"><a class="itemLink" href="#">
+                <img id="item${n}" src="${this.oItems[n].specialImage}" /></a></div>
                 `);
            }
         }
