ab39197e385fbb2f3f59f1e281b0ffe62a1a20e8
Cyrostims <a.j.cruickshank@rogers.com>
2020-09-29 09:03:13 -0400

    Update from Sept 15

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 import "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js";
 import "https://cdnjs.cloudflare.com/ajax/libs/marked/1.1.1/marked.js";
+import "https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js";
 import aPages from "../pages/index.js";
 
 class Page {
@@ -29,14 +30,22 @@ class Article extends Page {
             $("article").append(
                 `<section id="${aPages[n].title}"></section>`
             );
+            const sPage = aPages[n];
+            if (sPage.specialImage) {
+                $("article").append(`
+                <img src="${sPage.specialImage}"/>
+                `)
+            }
             new Section(aPages[n]).render();
         }
     }
 }
 
+const sName = "Alex Cruickshank";
+
 class Footer extends Page {
     render() {
-        const sName = "Alex Cruickshank";
+
         const yToday = new Date().getFullYear();
         $("footer").html(
             `&copy; ${yToday} ${sName}`
@@ -44,11 +53,41 @@ class Footer extends Page {
     }
 }
 
+class Nav extends Page {
+    render() {
+        let sMenu = "";
+        for (let n = 0; n < aPages.length; n++) {
+            const sMenuItem = aPages[n].title;
+            if (sMenuItem != "index") {
+                sMenu += `<li><a href="#${sMenuItem}">${sMenuItem}</a></li>`;
+            }
+        }
+        $("nav").html(`
+                <div class="navbar navbar-inverse navbar-static-top" role="navigation">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">Portfolio of ${sName}</a>
+                    </div>
+                    <div class="navbar-collapse collapse">
+                        <ul class="nav navbar-nav navbar-right">                       
+                            ${sMenu}                        
+                        </ul>
+                    </div>
+                </div>
+            `);
+    }
+}
+
 class Portfolio extends Page {
     constructor() {
         super();
         this.header = new Page();
-        this.nav = new Page();
+        this.nav = new Nav();
         this.article = new Article();
         this.footer = new Footer();
     }
--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,4 @@
-export default [{"title":"about", "fname":"about.md"},
-{"title":"index", "fname":"index.md"}];
+export default [{ "title": "About", "fname": "about.md", "specialImage": "https://cdn.pixabay.com/photo/2016/03/28/12/35/cat-1285634_960_720.png" },
+    { "title": "Contact", "fname": "contact.md" },
+    { "title": "index", "fname": "index.md" }
+];
