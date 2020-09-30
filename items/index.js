dfbaa5d42bed3678c2e4846449b05eee89b9b6f2
Cyrostims <a.j.cruickshank@rogers.com>
2020-09-29 22:30:32 -0400

    js fix attempt 4

---
+++ items/index.js
@@ -0,0 +1,5 @@
+export default [{ "id": "item1", "fname": "logo1.md", "specialImage": "/images/C.png" },
+    { "id": "item2", "fname": "logo2.md", "specialImage": "/images/Quasar.png" },
+    { "id": "item3", "fname": "logo3.md", "specialImage": "/images/Driverless_Car.png" },
+    { "id": "item4", "fname": "logo4.md", "specialImage": "/images/Whoosh.png" }
+];
